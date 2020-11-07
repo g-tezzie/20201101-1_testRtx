aee2d9b90136885d3535848bb9d126cd65c59832
gtezz <gtezz@DESKTOP-EBINUJQ.mshome.net>
2020-11-07 15:20:31 +0900

    cpp/cu hybrid

---
+++ 20201101-1_testRtx/Fir.h
@@ -0,0 +1,96 @@
+/*
+ * Fir.h
+ *
+ *  Created on: 2020/11/07
+ *      Author: gtezz
+ */
+
+#ifndef FIR_H_
+#define FIR_H_
+
+#include <iostream>
+#include <cmath>
+
+class Fir {
+	static const int depth = 12;
+	static const int taps = (1 << depth);
+	static const int mask = (1 << depth) - 1;
+	long long x[1 << depth];
+	long long k[1 << depth];
+	int ptr = 0;
+
+	void triangular();
+	void sinc();
+	void clear();
+
+public:
+	Fir() {
+		clear();
+		sinc();
+	}
+	virtual ~Fir() {
+		// TODO Auto-generated destructor stub
+	}
+
+	short int block(const short int u[]) {
+		long long a = 0;
+		for (int i = 0; i < 1 << depth; i++) {
+			a += u[i] * k[i];
+		}
+		return a >> 64 - 16;
+	}
+
+	short int put(short int u) {
+		int mptr = ptr & mask;
+		short int retval = x[mptr] >> 64 - 16;
+		x[mptr] = 0;
+		for (int i = 0; i < 1 << depth; i++) {
+			x[(ptr + i) & mask] += u * k[i];
+		}
+		ptr++;
+		return retval;
+	}
+};
+
+inline void Fir::triangular() {
+	for (int i = 0; i < taps; i++)
+		k[i] = 2.0 * (1LL << (64 - 16 - depth)) * (1.0 - (float) ((i)) / taps);
+}
+
+
+inline void Fir::sinc() {
+
+//	function [x]=filt_sinc(n,fl)
+
+//x=sinc(n,fl)
+//Calculate n samples of the function sin(2*pi*fl*t)/(pi*t)
+//for t=-n/2:n/2 (i.e. centered around the origin).
+//  n  :Number of samples
+//  fl :Cut-off freq. of assoc. low-pass filter in Hertz
+//  x  :Samples of the sinc function
+
+	int n = taps;
+	double fl = 200.0/48000.0;
+	double pi = atan2(0, -1);
+
+	double no2 = (n - 1) / 2;
+	double wl = fl * 2 * pi;
+
+	for (int i = 0; i < taps; i++) {
+		double no2Now = -no2 + i;
+		double xn = sin(wl * no2Now);
+		double xd = pi * no2Now;
+		double xx = no2Now == 0 ? 2 * fl : xn / xd;
+		k[i] = xx * (1LL << (64 - 16));
+
+//		std::cerr << i << ": " << xx << std::endl;
+	}
+
+}
+
+inline void Fir::clear() {
+	for (int i = 0; i < taps; i++)
+		x[i] = 0;
+}
+
+#endif /* FIR_H_ */
---
+++ 20201101-1_testRtx/Source.cpp
@@ -0,0 +1,72 @@
+//============================================================================
+// Name        : testCpp.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <fstream>
+#include <iostream>
+#include <ctime>
+
+#include "Fir.h"
+
+using namespace std;
+
+const int buflen = 100e6;
+char ioRaw[buflen];
+short int raw[2][buflen / 4];
+short int rawo[2][buflen / 4];
+
+int main() {
+	time_t start = time(nullptr);
+
+	//	ifstream in = ifstream("in.raw", std::ios::binary);
+	basic_ifstream<char> in = basic_ifstream<char>("in.raw", std::ios::binary);
+	ofstream out = ofstream("out.raw", std::ios::binary);
+
+	in.read(ioRaw, buflen);
+	int samples = in.gcount();
+	cerr << samples << endl;
+
+	Fir lFir = Fir();
+	Fir rFir = Fir();
+
+	for (int i = 0; i < (samples >> 2); i++) {
+		char ll, lh, rl, rh;
+		ll = ioRaw[i * 4];
+		lh = ioRaw[i * 4 + 1];
+		rl = ioRaw[i * 4 + 2];
+		rh = ioRaw[i * 4 + 3];
+
+		raw[0][i] = static_cast<short int>(lh << 8) + ll;
+		raw[1][i] = static_cast<short int>(rh << 8) + rl;
+	}
+
+#pragma loop(hint_parallel(0))
+#pragma loop(ivdep) // ivdep will force this through.
+	for (int i = 0; i < (samples >> 2); i++) {
+		rawo[0][i] = lFir.block(&raw[0][i]);
+		rawo[1][i] = rFir.block(&raw[1][i]);
+	}
+
+	for (int i = 0; i < (samples >> 2); i++) {
+
+		ioRaw[i * 4] = rawo[0][i];
+		ioRaw[i * 4 + 1] = (rawo[0][i] >> 8);
+		ioRaw[i * 4 + 2] = (rawo[1][i]);
+		ioRaw[i * 4 + 3] = (rawo[1][i] >> 8);
+	}
+
+	out.write(ioRaw, samples);
+
+	cerr << difftime(time(nullptr), start) << endl;
+
+	cerr << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+
+	extern int maine();
+	maine();
+
+	return 0;
+}
